658c608ea5ed5c6569c755268b1993eeaeec4355
WuRong1997 <49945846+WuRong1997@users.noreply.github.com>
2021-07-14 16:55:56 +0800

    Create args.py

---
+++ args.py
@@ -0,0 +1,13 @@
+import argparse
+
+def parse_commandline():
+    parser = argparse.ArgumentParser(description='---------------------')
+    parser.add_argument("--k_path", type=str, default="/one/two", help="keyword path")
+    args = parser.parse_args()
+    return args
+    
+if __name__ == '__main__':
+    args = parse_commandline()
+    with open(args.k_path, 'r') as fr:
+
+# 命令行中
